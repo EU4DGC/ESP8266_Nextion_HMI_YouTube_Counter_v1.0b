2ff701494d710872a79b1c4efbac94fe45e16dd4
Pavel Hladkikh <eu4dgc@gmail.com>
2019-03-07 15:02:04 +0300

    Update youtube_counter_v1.0b.ino
    В 39 строчке добавил client.setInsecure(); В 2019 году без нее не работает.

--- youtube_counter_v1.0b.ino
+++ youtube_counter_v1.0b.ino
@@ -36,7 +36,7 @@ void setup() {
   Serial1.begin(115200);// Скорость подключения к Nextion 
   Serial.flush();
   Serial1.flush();
-
+  client.setInsecure();
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(100);
